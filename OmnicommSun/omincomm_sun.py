7e3d4723d13e3583d8f181c406ba0069d710611c
Alexander <sun5038@yandex.ru>
2023-11-10 13:28:06 +0300

    Проба сделать библиотеку

--- OmnicommSun/omincomm_sun.py
+++ OmnicommSun/omincomm_sun.py
@@ -6,19 +6,25 @@ class SUN:
     URL = "https://config.omnicomm.ru/api"
 
     def __init__(self):
-        self.start()
+        self._start()
 
-    def start(self):
+    def _start(self):
         params = f'{self.URL}?action=locale&lang=ru'
         r.post(params)
 
-    def get_task_queue(self):
+    def _get_task_queue(self):
         data = {
             "action": "getTaskQueue"
         }
         response = r.post(url=self.URL, data=data)
+        return response
 
-    def get_registrator(self, terminal_id: int, password: str):
+    def get_registrator_settings(self, terminal_id: int, password: str):
+        """
+        :param terminal_id: номер терминала
+        :param password: пароль
+        :return: настройки терминала
+        """
         data = {
             "action": "getRegistrator",
             "data": json.dumps({
@@ -28,11 +34,10 @@ class SUN:
             })
         }
         response = r.post(url=self.URL, params=data)
-        print(response.json())
+        return response
 
 
 if __name__ == '__main__':
     sun = SUN()
-    # sun.get_task_queue()
     sun.get_registrator(254002001, '56987')
 
